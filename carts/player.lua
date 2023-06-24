426e11e3c8a726e16edac034b2df94b7b32acf8f
bardunbar <ian@dunbargames.com>
2023-06-24 14:45:26 +0100

    Add wall collision

--- carts/player.lua
+++ carts/player.lua
@@ -3,26 +3,38 @@ local gravity = 7 * 1/60
 player = {
     x = 64,
     y = 64,
-    v_x = 0,
-    v_y = 0,
+    dx = 0,
+    dy = 0,
     grounded = false,
     moving = false,
     direction = 0,
     currentFrame = 0
+
 }
 
 function player:update()
 
+    local startx = self.x
+
     -- Update Velocity
+    self.dx=0
+    if btn(0) then --left
+        self.dx=-2
+    end
+    if btn(1) then --right
+        self.dx=2
+    end
+
+
 
     -- Apply Gravity
     if not self.grounded then
-        self.v_y += gravity
+        self.dy += gravity
     end
 
     -- Update Position
-    self.x += self.v_x
-    self.y += self.v_y
+    self.x += self.dx
+    self.y += self.dy
 
     -- Solve Collision
     local val = mget((self.x+4)/8,(self.y+8)/8)
@@ -30,6 +42,23 @@ function player:update()
         self.y = flr((self.y)/8)*8
         self.grounded = true
     end
+
+    local xoffset=0
+    if self.dx>0 then xoffset=7 end
+
+    local h=mget((self.x + xoffset) / 8, (self.y + 7) / 8)
+    if fget(h,0) then
+        --they hit a wall so move them
+        --back to their original pos.
+        --it should really move them to
+        --the edge of the wall but this
+        --mostly works and is simpler.
+        self.x = startx
+    end
+
+    -- Set animation state variables
+
+
 end
 
 function player:draw()
@@ -39,5 +68,5 @@ end
 
 function player:reset_position(x, y)
     self.x, self.y = x, y
-    self.v_x, self.v_y = 0, 0
+    self.dx, self.v_y = 0, 0
 end
