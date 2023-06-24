d7b3cb265004500e148cf973cbbb12720092e3cc
Dave Ryley <daaryley@me.com>
2023-06-24 14:05:29 +0100

    Adding callbacks to buttons in menu

--- carts/mainMenu.lua
+++ carts/mainMenu.lua
@@ -1,10 +1,21 @@
 mainMenu = {}
 
+local function onStartGame()
+
+end
+
+local function onOpenCredits()
+
+end
+
 function mainMenu:init(inGameContext)
     self.gameContext = inGameContext
 
     self.highlightedButton = 1
-    self.buttonOptions = { "Start Game", "Credits", "Another Button" }
+    self.buttonOptions = { 
+        { text = "START GAME", onPress = onStartGame }, 
+        { text = "CREDITS", onPress = onOpenCredits}, 
+    }
 end
 
 function mainMenu:onEnterScreen()
@@ -50,25 +61,40 @@ function mainMenu:downInput()
     end
 end
 
+function mainMenu:zInputPressed()
+
+end
+
+function mainMenu:zInputReleased()
+    self.buttonOptions[self.highlightedButton].onPress()
+end
+
 function mainMenu:update()
     if btn(2) then
         self:upInput()
     elseif btn(3) then
         self:downInput()
     end
+    if btn(4) and not self.lastZInput then
+        self:zInputPressed()
+    elseif not btn(4) and self.lastZInput then
+        self:zInputReleased()
+    end
 
+    -- store inputs for next update
     self.lastUpInput = btn(2)
     self.lastDownInput = btn(3)
+    self.lastZInput = btn(4)
 end
 
 function mainMenu:draw()
     updateHighlightColor()
-    print("Out of Bounds", 30, 30, 7)
+    print("out of bounds", 30, 30, 7)
 
     for i = 1, #self.buttonOptions do
         local isHighlighted = i == self.highlightedButton
         local prefix = isHighlighted and " > " or "  "
-        local color = isHighlighted and highlightColor or 6
-        print(prefix .. self.buttonOptions[i], xoffset, yoffset*i + ystart, color)
+        local color = (isHighlighted and self.lastZInput and 7) or (isHighlighted and highlightColor) or 6
+        print(prefix .. self.buttonOptions[i].text, xoffset, yoffset*i + ystart, color)
     end
 end
